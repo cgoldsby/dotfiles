d6893742b2205d413d45060a1d97a458133dc181
cgoldsby <cgoldsby@gmail.com>
2017-01-27 17:35:08 -0500

    Update setup to set wallpaper

--- setup.sh
+++ setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+puts () {
+	echo "✓ $1"
+}
+
 install_zsh () {
 	# Test to see if zshell is installed.
 	if [ ! -f /bin/zsh -o -f /usr/bin/zsh ]; then
 		sudo apt-get install -y git zsh
-		echo "✓ zsh installed"
+		puts "zsh installed"
 	fi
 
 	# Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-      chsh -s $(which zsh)
-      echo "✓ zsh set as default shell"
-    fi
+	if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+		chsh -s $(which zsh)
+      		puts "zsh set as default shell"
+    	fi
 
 	if [ ! -d $HOME/.zprezto ]; then
     	zprezto/setup.zsh
@@ -21,7 +25,7 @@ install_zsh () {
 setup_zprezto () {
 	cp zprezto/zpreztorc $HOME/.zpreztorc
 	cp zprezto/themes/prompt_cgoldsby_setup $HOME/.zprezto/modules/prompt/functions
-	echo "✓ zprezto configured"
+	puts "zprezto configured"
 }
 
 setup_iterm () {
@@ -29,16 +33,22 @@ setup_iterm () {
 	defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 	defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 	defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${HOME}/Sources/dotfiles/iterm"
-	echo "✓ iTerm configured"
+	puts "iTerm configured"
 }
 
 setup_shell (){
-	cp shell/bashrc ~/.bashrc
-	cp shell/zshrc ~/.zshrc
-	echo "✓ shell configured"
+	ln -sf ${HOME}/Sources/dotfiles/shell/bashrc ~/.bashrc
+	ln -sf ${HOME}/Sources/dotfiles/shell/zshrc ~/.zshrc
+	puts "shell configured"
+}
+
+setup_desktop () {
+	osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/cgoldsby/Sources/dotfiles/desktop/wallpaper.jpg"'
+	puts "desktop configured"
 }
 
 install_zsh
 setup_zprezto
 setup_iterm
 setup_shell
+setup_desktop
