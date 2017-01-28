259a723e07206a4f2a78cac0b401060b46edf4ab
cgoldsby <cgoldsby@gmail.com>
2017-01-28 06:15:40 -0500

    Rename puts to log in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-puts () {
+logs () {
 	echo "✓ $1"
 }
 
@@ -8,13 +8,13 @@ install_zsh () {
 	# Test to see if zshell is installed.
 	if [ ! -f /bin/zsh -o -f /usr/bin/zsh ]; then
 		sudo apt-get install -y git zsh
-		puts "zsh installed"
+		logs "zsh installed"
 	fi
 
 	# Set the default shell to zsh if it isn't currently set to zsh
 	if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
 		chsh -s $(which zsh)
-      		puts "zsh set as default shell"
+      		logs "zsh set as default shell"
     	fi
 
 	if [ ! -d $HOME/.zprezto ]; then
@@ -25,7 +25,7 @@ install_zsh () {
 setup_zprezto () {
 	ln -sf $HOME/Sources/dotfiles/zprezto/zpreztorc $HOME/.zpreztorc
 	cp zprezto/themes/prompt_cgoldsby_setup $HOME/.zprezto/modules/prompt/functions
-	puts "zprezto configured"
+	logs "zprezto configured"
 }
 
 setup_iterm () {
@@ -33,24 +33,24 @@ setup_iterm () {
 	defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 	defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 	defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${HOME}/Sources/dotfiles/iterm"
-	puts "iTerm configured"
+	logs "iTerm configured"
 }
 
 setup_terminal () {
 	defaults write $HOME/Library/Preferences/com.apple.Terminal.plist "Default Window Settings" -string "Pro"
 	defaults write $HOME/Library/Preferences/com.apple.Terminal.plist "Startup Window Settings" -string "Pro"
-	puts "terminal configured"
+	logs "terminal configured"
 }
 
 setup_shell () {
 	ln -sf $HOME/Sources/dotfiles/shell/bashrc $HOME/.bashrc
 	ln -sf $HOME/Sources/dotfiles/shell/zshrc $HOME/.zshrc
-	puts "shell configured"
+	logs "shell configured"
 }
 
 setup_desktop () {
 	osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/cgoldsby/Sources/dotfiles/desktop/wallpaper.jpg"'
-	puts "desktop configured"
+	logs "desktop configured"
 }
 
 install_zsh
