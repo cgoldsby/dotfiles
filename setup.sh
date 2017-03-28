15a300437c9abe413142dd998b6f594760518e2b
cgoldsby <cgoldsby@gmail.com>
2017-03-28 16:54:37 -0400

    Added homebrew install and cleaned up setup

--- setup.sh
+++ setup.sh
@@ -14,21 +14,36 @@ install_zsh () {
 	# Set the default shell to zsh if it isn't currently set to zsh
 	if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
 		chsh -s $(which zsh)
-      		logs "zsh set as default shell"
-    	fi
+      logs "zsh set as default shell"
+    fi
 
 	if [ ! -d $HOME/.zprezto ]; then
 		zprezto/setup.zsh
 	fi
 }
 
+install_brew () {
+	if [ ! -f "`which brew`" ]; then
+	  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		logs "homebrew installed"
+	else
+	  brew update
+		logs "homebrew updated"
+	fi
+}
+
 install_m-cli () {
- curl -fsSL https://raw.githubusercontent.com/rgcr/m-cli/master/install.sh | sudo sh
- logs "m-cli installed (or updated)"
+	if [ ! -f "`which m`" ]; then
+	  brew install m-cli
+	  logs "m-cli installed"
+	else
+		brew upgrade --cleanup m-cli
+		logs "m-cli upgraded"
+	fi
 }
 
 setup_zprezto () {
-	ln -sf $HOME/Sources/dotfiles/zprezto/zpreztorc $HOME/.zpreztorc
+	ln -sf `pwd`/zprezto/zpreztorc $HOME/.zpreztorc
 	cp zprezto/themes/prompt_cgoldsby_setup $HOME/.zprezto/modules/prompt/functions
 	logs "zprezto configured"
 }
@@ -37,7 +52,7 @@ setup_iterm () {
 	# Don’t display the annoying prompt when quitting iTerm
 	defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 	defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
-	defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${HOME}/Sources/dotfiles/iterm"
+	defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$(pwd)/iterm"
 	logs "iTerm configured"
 }
 
@@ -48,8 +63,8 @@ setup_terminal () {
 }
 
 setup_shell () {
-	ln -sf $HOME/Sources/dotfiles/shell/bashrc $HOME/.bashrc
-	ln -sf $HOME/Sources/dotfiles/shell/zshrc $HOME/.zshrc
+	ln -sf `pwd`/shell/bashrc $HOME/.bashrc
+	ln -sf `pwd`/shell/zshrc $HOME/.zshrc
 	logs "shell configured"
 }
 
@@ -60,14 +75,24 @@ setup_desktop () {
 
 setup_finder () {
   chflags nohidden ~/Library/
+	m finder showextensions YES
   logs "finder configured"
 }
 
+setup_dock () {
+	m dock autohide NO
+	m dock magnification NO
+	m dock position BOTTOM
+	logs "dock configured"
+}
+
 install_zsh
+install_brew
+install_m-cli
 setup_zprezto
 setup_iterm
 setup_shell
 setup_terminal
-install_m-cli
 setup_desktop
 setup_finder
+setup_dock
